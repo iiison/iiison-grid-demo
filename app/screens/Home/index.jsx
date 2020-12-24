3ad2e3bb47e8e524f2db1e81f70ab041cb5d638f
iiison <i.bharat.soni@gmail.com>
2020-12-25 07:05:26 +0800

    FEAT: Add home page grid layout
    - Add base styles for home page grid
    - Add grid items placeholders
    
    Home Page flow

--- app/screens/Home/index.jsx
+++ app/screens/Home/index.jsx
@@ -1,9 +1,62 @@
 import React from 'react'
+import { PropTypes } from 'prop-types'
+
+import { Button } from '$COMPONENTS'
+import styles from './styles.css'
+
+const GridItem = ({ children, imageUrl, size }) => {
+  const sizeClass = styles[`span_${size}`]
+
+  return (
+    <div className={`${styles.gridItem} ${sizeClass}`}>
+      <img className={styles.heroImg} src={imageUrl} />
+      {children}
+    </div>
+  )
+}
+
+GridItem.propTypes = {
+  children : PropTypes.node,
+  imageUrl : PropTypes.string,
+  size     : PropTypes.oneOf(['1_1', '1_2', '2_1', '2_2'])
+}
 
 const Home = () => {
   return (
-    <div className='grid main-grid'>
-      This is home.
+    <div className={`grid ${styles.homeGrid}`}>
+      <GridItem size='2_2'>
+        <div className={`grid-middle grid-center full-height t-upper`}>
+          <div>
+            <p className={`${styles.headingXL}`}>Play<br />Win<br /> Excite</p>
+            <Button type='primary'>Get Started!</Button>
+          </div>
+        </div>
+      </GridItem>
+      <GridItem size='1_1' />
+      <GridItem size='1_1' />
+      <GridItem size='1_1' />
+      <GridItem size='1_1' />
+      <GridItem size='1_2'>
+        <div className={`grid-middle grid-center full-height t-upper`}>
+          <p className={`${styles.headingL}`}>Live Games<br />With Friends</p>
+        </div>
+      </GridItem>
+      <GridItem size='1_1' />
+      <GridItem size='1_1' />
+      <GridItem size='1_1' />
+      <GridItem size='1_1'>
+        <div className={`grid-middle grid-center full-height t-upper`}>
+          <p className={`${styles.headingL}`}>Esports</p>
+        </div>
+      </GridItem>
+      <GridItem size='1_2'>
+        <div className={`grid-middle grid-center full-height t-upper`}>
+          <div className='col grid-center'>
+            <p className={`${styles.heading}`}>Love Playing?<br />Join the League</p>
+            <a href='#' className={`t-right t-capitalize ${styles.applyLink}`}>Apply Now!</a>
+          </div>
+        </div>
+      </GridItem>
     </div>
   )
 }
