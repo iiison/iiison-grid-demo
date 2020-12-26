3c1851df6c9a237d750c15e9788a4730b8efbb4d
iiison <i.bharat.soni@gmail.com>
2020-12-26 12:25:06 +0800

    FEAT: Use JSON file to render the Home page grid
    Final touches

--- app/screens/Home/index.jsx
+++ app/screens/Home/index.jsx
@@ -3,15 +3,42 @@ import { PropTypes } from 'prop-types'
 
 import { Button } from '$COMPONENTS'
 import styles from './styles.css'
+import data from './data.json'
 
-const GridItem = ({ children, imageUrl, size }) => {
+const GridItem = ({
+  size,
+  link,
+  imgUrl,
+  button,
+  content,
+  children,
+}) => {
   const sizeClass = styles[`span_${size}`]
+  let contentClasses = ''
+
+  if (content) {
+    const contentSizeClassName = `heading${content.size.toUpperCase()}`
+    contentClasses = `${styles[contentSizeClassName]} ${content.shouldCenter && 't-center'}`
+  }
 
   return (
     <div className={`${styles.gridItem} ${sizeClass} grid-middle`}>
-      <img className={styles.heroImg} src={imageUrl} />
+      <img className={styles.heroImg} src={imgUrl} />
       <div className={styles.content}>
         {children}
+        {content && (
+          <div className={`grid-middle grid-center full-height t-upper`}>
+            <div>
+              {content.text.split('\n').map(text => (
+                <p className={`${contentClasses}`} key={text}>{text}</p>)
+              )}
+              {button && <Button type='primary'>{button.text}</Button>}
+            </div>
+            {link && (
+              <a href={link.url} className={`t-right t-capitalize col-12 ${styles.applyLink}`}>{link.text}</a>
+            )}
+          </div>
+        )}
       </div>
     </div>
   )
@@ -19,46 +46,26 @@ const GridItem = ({ children, imageUrl, size }) => {
 
 GridItem.propTypes = {
   children : PropTypes.node,
-  imageUrl : PropTypes.string,
-  size     : PropTypes.oneOf(['1_1', '1_2', '2_1', '2_2'])
+  imgUrl   : PropTypes.string.isRequired,
+  size     : PropTypes.oneOf(['1_1', '1_2', '2_1', '2_2']).isRequired,
+  content  : PropTypes.shape({
+    text : PropTypes.string,
+    shouldCenter : PropTypes.bool,
+    size : PropTypes.oneOf(['xl', 'l', 's']),
+  }),
+  button : PropTypes.shape({
+    text : PropTypes.string
+  }),
+  link : PropTypes.shape({
+    url  : PropTypes.string,
+    text : PropTypes.string
+  })
 }
 
 const Home = () => {
   return (
     <div className={`grid ${styles.homeGrid}`}>
-      <GridItem size='2_2' imageUrl='https://i.imgur.com/yJAsBlY.jpg'>
-        <div className={`grid-middle grid-center full-height t-upper`}>
-          <div>
-            <p className={`${styles.headingXL}`}>Play<br />Win<br /> Excite</p>
-            <Button type='primary'>Get Started!</Button>
-          </div>
-        </div>
-      </GridItem>
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_2' imageUrl='https://i.imgur.com/yJAsBlY.jpg'>
-        <div className={`grid-middle grid-center full-height t-upper`}>
-          <p className={`${styles.headingL}`}>Live Games<br />With Friends</p>
-        </div>
-      </GridItem>
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg' />
-      <GridItem size='1_1' imageUrl='https://i.imgur.com/yJAsBlY.jpg'>
-        <div className={`grid-middle grid-center full-height t-upper`}>
-          <p className={`${styles.headingL}`}>Esports</p>
-        </div>
-      </GridItem>
-      <GridItem size='1_2' imageUrl='https://i.imgur.com/yJAsBlY.jpg'>
-        <div className={`grid-middle grid-center full-height t-upper`}>
-          <div className='col grid-center'>
-            <p className={`${styles.heading}`}>Love Playing?<br />Join the League</p>
-            <a href='#' className={`t-right t-capitalize ${styles.applyLink}`}>Apply Now!</a>
-          </div>
-        </div>
-      </GridItem>
+      {data.map((gridItemData, index) => <GridItem key={index} {...gridItemData} /> )}
     </div>
   )
 }
